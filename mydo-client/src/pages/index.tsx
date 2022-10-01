73ae18233cbe652dcb5afd57f77e5915fc144882
Cone <773768893@qq.com>
2022-10-01 15:38:12 +0800

    feat: init

---
+++ mydo-client/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import { Button } from 'tdesign-react';
+import 'tdesign-react/es/style/index.css';
+import styles from './index.less';
+
+export default function IndexPage() {
+  return (
+    <div>
+			<Button
+				shape="rectangle"
+				size="medium"
+				type="button"
+				variant="base"
+	>
+        1
+      </Button>
+    </div>
+  );
+}
